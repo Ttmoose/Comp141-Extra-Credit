5e0ba64a28e3f2aae12625278d686305bd815c5b
Ubuntu <comp141@ML-RefVm-949980.zv0ixxhdtw1epaxq4zps5kvxsc.ex.internal.cloudapp.net>
2023-12-07 20:02:50 +0000

    Extra Credit

--- Main.sh
+++ Main.sh
@@ -1,3 +1,20 @@
-echo Tuker's Comp141-Extra-Credit
-echo All done!
+mkdir Output
+cp Textfile.txt Output
+cd Output
+cat Textfile.txt > read.txt
+pwd > pwd.txt
+ls > ls.txt
+cp Textfile.txt copy.txt
+alias print_date='date'
+print_date > date.txt
+wc -w Textfile.txt > textcount.txt
+ps | head -n 5 > process.txt
+ip | head -n 5 > netstat.txt
+mount | head -n 5 > mount.txt
+touch permissions.txt
+chmod a+rwx permissions.txt
+TESTENV='test'
+grep -E '\b\w{3,}\b'
+cd ..
+
 

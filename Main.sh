76d474396224fd88a27bc9cc4ee060afa4b72697
Ttmoose <150546784+Ttmoose@users.noreply.github.com>
2023-12-07 14:40:21 -0600

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -5,8 +5,8 @@ cat Textfile.txt > read.txt
 pwd > pwd.txt
 ls > ls.txt
 cp Textfile.txt copy.txt
-alias date='printdate'
-printdate > date.txt
+alias date='pd'
+pd > date.txt
 wc -w Textfile.txt > textcount.txt
 ps -ef | head -n 5 > process.txt
 ip a | head -n 5 > netstat.txt

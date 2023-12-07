0cc3e9e4311524c29fb24841e4655a52b8a44d0c
Ubuntu <comp141@ML-RefVm-949980.zv0ixxhdtw1epaxq4zps5kvxsc.ex.internal.cloudapp.net>
2023-12-07 20:28:20 +0000

    Extra-Credit

--- Main.sh
+++ Main.sh
@@ -5,16 +5,16 @@ cat Textfile.txt > read.txt
 pwd > pwd.txt
 ls > ls.txt
 cp Textfile.txt copy.txt
-alias print_date='date'
-print_date > date.txt
+alias date='printdate'
+printdate > date.txt
 wc -w Textfile.txt > textcount.txt
-ps | head -n 5 > process.txt
-ip | head -n 5 > netstat.txt
+ps -ef | head -n 5 > process.txt
+ip a | head -n 5 > netstat.txt
 mount | head -n 5 > mount.txt
 touch permissions.txt
 chmod a+rwx permissions.txt
 TESTENV='test'
-grep -E '\b\w{3,}\b'
+grep -E -o '\b\w{3,}\b' Textfile.txt > regex.txt
 cd ..
 
 
